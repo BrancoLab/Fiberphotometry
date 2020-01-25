67f2f2136bc9ff3476158fd2f074cfdeddf04c56
FedeClaudi <federicoclaudi@protonmail.com>
2020-01-25 17:45:08 +0000

    bump

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -15,55 +15,75 @@ from utils.maths.filtering import *
 from utils.maths.stimuli_detection import *
 from utils.colors import *
 
-left_led_color = darkturquoise
-right_led_color = orange
-both_leds_color = salmon
-both_leds_off_color = darkseagreen
-
+mvmtc = darkturquoise
+ldrc = salmon
 blueled = lightskyblue
 violetled = violet
-
+ldr_onc = orange
+ldr_offc = darkorange
 
 
 # %%
-main_fld = "Z:\\swc\\branco\\rig_photometry\\191203"
-exp_fld = "191203_CE972_4"
+main_fld = "Z:\\swc\\branco\\rig_photometry\\191213"
+exp_flds = ["191213_994832_right_2", "191213_994832_right_3"]
 
-data = pd.read_csv(os.path.join(main_fld, exp_fld, "sensors_data.csv"))
-b, v = data.ch_0_motion.values[2:], data.ch_0_signal.values[2:]
-left, right = data.left_led_on.values[2:], data.right_led_on.values[2:]
+b, v, mvmt, ldr = [], [], [], []
+for f in exp_flds:
+    data = pd.read_csv(os.path.join(main_fld, f, "sensors_data.csv")) 
+    b.extend(list(data.ch_0_motion.values[2:]))
+    v.extend(list(data.ch_0_signal.values[2:]))
+    mvmt.extend(list(data.behav_mvmt.values[2:]))
+    ldr.extend(list(data.ldr.values[2:]))
+
+b, v, mvmt, ldr = np.array(b), np.array(v), np.array(mvmt), np.array(ldr)
 # %%
-f, axarr = plt.subplots(nrows=2)
+f, axarr = plt.subplots(nrows=4, figsize=(16, 12))
 
-axarr[0].plot(v)
-axarr[1].plot(b)
-
+axarr[0].plot(b, color=blueled)
+axarr[1].plot(v, color=violetled)
+axarr[2].plot(mvmt, color=mvmtc)
+axarr[3].plot(ldr, color=ldrc)
 # %%
-f, ax = plt.subplots()
+f, ax = plt.subplots(figsize=(16, 12))
 ax.scatter(v, b, alpha=.3)
 
 regressor = LinearRegression()  
 regressor.fit(v.reshape(-1, 1), b.reshape(-1, 1)) #training the algorithm
-
+expected_b = v*regressor.coef_[0][0] + regressor.intercept_[0]
+corrected_b = b-expected_b
 
 print("intercept", regressor.intercept_[0]) 
 print("slope", regressor.coef_[0])
 
-# %%
 
+# %%
+# Stimuli detection
+ldr2 = np.zeros_like(ldr)
+ldr2[ldr>4.45] = 1
+
+ldr_onset = np.where(np.diff(ldr2) > .5)[0]
+ldr_offset = np.where(np.diff(ldr2) < -.5)[0]
+
+
+f, ax = plt.subplots(figsize=(16, 12))
+
+
+ax.plot(np.diff(ldr2), color=ldrc)
+ax.scatter(ldr_onset, [.1 for on in ldr_onset], color=ldrc)
+ax.scatter(ldr_offset, [-.1 for on in ldr_offset], color=ldrc)
+
+print(len(ldr_onset), len(ldr_offset))
+
+# %%
 # ---------------------------------------------------------------------------- #
 #                                  ! MAIN PLOT                                 #
 # ---------------------------------------------------------------------------- #
 
-expected_b = v*regressor.coef_[0][0] + regressor.intercept_[0]
-corrected_b = b-expected_b
 
-left_stim_onsets = np.where(np.diff(left) > .5)[0]
-right_stim_onsets = np.where(np.diff(right) > .5)[0]
-left_stim_offsets = np.where(np.diff(left) < -.5)[0]
-right_stim_offsets = np.where(np.diff(right) < -.5)[0]
 
-f, axarr = plt.subplots(figsize=(18, 4), nrows=4)
+shift = 20
+
+f, axarr = plt.subplots(figsize=(18, 12), nrows=4)
 
 axarr[0].plot(b, color=blueled, zorder=99)
 axarr[1].plot(v, color=violetled, zorder=99)
@@ -71,82 +91,54 @@ axarr[1].plot(v, color=violetled, zorder=99)
 axarr[2].plot(corrected_b, color='k', alpha=.8)
 axarr[2].plot(line_smoother(corrected_b), color='r', lw=2, zorder=99)
 
-axarr[3].plot(left, color=left_led_color, lw=2)
-axarr[3].plot(right, color=right_led_color, lw=2)
+axarr[3].plot(ldr2, color=ldrc, lw=2)
 
-for n, g in enumerate(left_stim_onsets):
+
+for g in ldr_onset:
     for i in [0, 1, 2]:
-        if n%2 == 0:
-            axarr[i].axvline(g, ls="--", color=left_led_color)
-        else:
-            axarr[i].axvline(g, ls="--", color=both_leds_color)
+        axarr[i].axvline(g, ls="--", color=ldr_onc)
 
-for g in right_stim_onsets:
+for g in ldr_offset:
     for i in [0, 1, 2]:
-        axarr[i].axvline(g, ls="--", color=right_led_color)
+        axarr[i].axvline(g, ls="--", color=ldr_offc)
 
-for g in right_stim_offsets:
-    for i in [0, 1, 2]:
-        axarr[i].axvline(g, ls="--", color=both_leds_off_color)
-
-
-axarr[3].scatter(left_stim_onsets, [1.1 for i in left_stim_onsets], color=left_led_color, s=50)
-axarr[3].scatter(right_stim_onsets, [1.2 for i in right_stim_onsets], color=right_led_color, s=50)
-axarr[3].scatter(left_stim_offsets, [-.1 for i in left_stim_onsets], color=left_led_color, s=50)
-axarr[3].scatter(right_stim_offsets, [-.2 for i in right_stim_onsets], color=right_led_color, s=50)
-
-
-# %%
-# AVG per LED on
-both_off = np.where((left == 0) & (right == 0))
-left_on = np.where(left == 1)
-right_on = np.where(right == 1)
-both_on = np.where((left == 1) & (right == 1))
-
-f, ax = plt.subplots()
-for i, idx in enumerate([both_off, left_on, right_on, both_on]):
-    ax.errorbar(i, np.mean(corrected_b[idx]), fmt='o', yerr=stats.sem(corrected_b[idx]), color='b')
-_ = ax.set(xticks=np.arange(4), xticklabels=[ 'none', 'left', 'right', 'both',])
 
 # %%
 # Traces for LED on
-preframes, postframes = 40, 40
-f, axarr = plt.subplots(nrows=4, figsize=(10, 10))
+preframes, postframes = 80, 100
+stimdur=26
+f, ax = plt.subplots(figsize=(16, 5), sharex=True, )
 
-ll, rr, both, both_off = [], [], [], []
+on, off = [], [], 
 
-for i,l in enumerate(left_stim_onsets):
-    if i % 2 == 0:
-        axarr[0].plot(corrected_b[l-preframes:l+postframes], color=cyan)
-        ll.append(corrected_b[l-preframes:l+postframes])
-    else:
-        axarr[2].plot(corrected_b[l-preframes:l+postframes], color=orange)
-        both.append(corrected_b[l-preframes:l+postframes])
-
-for i, r in enumerate(right_stim_onsets):
-    axarr[1].plot(corrected_b[r-preframes:r+postframes], color=darkturquoise)
-    rr.append(corrected_b[r-preframes:r+postframes])
+for i,l in enumerate(ldr_onset):
+    # axarr[0].plot(corrected_b[l-preframes:l+postframes], color=cyan, alpha=.3)
+    on.append(corrected_b[l-preframes:l+postframes])
 
 
-for i, o in enumerate(right_stim_offsets):
-    axarr[3].plot(corrected_b[o-preframes:o+postframes], color=darkred)
-    both_off.append(corrected_b[o-preframes:o+postframes])
+for i,l in enumerate(ldr_offset):
+    # axarr[1].plot(corrected_b[l-preframes:l+postframes], color=red, alpha=.3)
+    off.append(corrected_b[l-preframes:l+postframes])
+    
+ax.plot(np.mean(np.vstack(on), axis=0), color=seagreen, lw=4, zorder=99)
 
-axarr[0].plot(np.mean(np.vstack(ll), axis=0), color='k', lw=3)
-axarr[1].plot(np.mean(np.vstack(rr), axis=0), color='k', lw=3)
-axarr[2].plot(np.mean(np.vstack(both), axis=0), color='k', lw=3)
-axarr[3].plot(np.mean(np.vstack(both_off), axis=0), color='k', lw=3)
+ax.fill_between(np.arange(len(on[0])), np.mean(np.vstack(on), axis=0)-stats.sem(np.vstack(on), axis=0), np.mean(np.vstack(on), axis=0)+stats.sem(np.vstack(on), axis=0), 
+                        color='k', alpha=.3)
 
-for ax in list(axarr):
-    ax.axvline(preframes, color='k')
+# axarr[2].plot(np.mean(np.vstack(both), axis=0), color='k', lw=3)
+# axarr[3].plot(np.mean(np.vstack(both_off), axis=0), color='k', lw=3)
 
-_ =axarr[0].set(title='Left LED onset aligned')
-_ =axarr[1].set(title='Right LED onset aligned')
-_ =axarr[2].set(title='Both LED onset aligned')
-_ =axarr[3].set(title='Both LED offset aligned')
 
-f.tight_layout()
+ax.axvline(preframes, color=salmon, lw=4, ls="--", alpha=.6)
+ax.axvline(preframes+stimdur, color=salmon, lw=4, ls="--", alpha=.6)
 
+
+X = np.arange(0, 180, 16)
+_ = ax.set(title='GRATING ALIGNED', ylabel='Normalised fluorescence', xlabel="time(s)", ylim=[-2, 4],
+                xticks=X, xticklabels=((X-80)/16).astype(np.int8))
+
+# f.tight_layout()
+# 
 
 
 
