46196bd52b704a1144d98b1034f4ea8358b27d67
FedeClaudi <federicoclaudi@protonmail.com>
2020-02-06 09:06:16 +0000

    refactoring pipeline

--- fiberphotometry/preprocessing/pipeline.py
+++ fiberphotometry/preprocessing/pipeline.py
@@ -21,7 +21,7 @@ class Pipeline:
     def __init__(self, data_folder, ca_video_tdms,
                     ca_video_path=None, fps=100,
                     overwrite=False, run_conversion=True,
-                    extract_frame_times=True, **kwargs):
+                    extract_frame_times=True, logging=False, **kwargs):
         """
             Pipeline to preprocess fiberphotometry data. IT:
                 1) Converts video.tdms to .mp4
@@ -36,144 +36,163 @@ class Pipeline:
             :param overwrite: bool, if true it overwrites the results of previous preprocessing
             :param extract_frame_times: bool, if true it extracts the time of camera, blue led and violet led frames
                         from analog inputs files.
+            :param logging: set as true if fancylog logging was started by another application. 
             :param kwargs: used to pass params for extract_frame_times: [analog_inputs_tdms, camera_triggers_channel,
                         bue_led_triggers_channel, violet_led_triggers_channel]
             
         """
+        # Params
+        self.overwrite = overwrite
+        self.run_conversion = run_conversion
+        self.data_folder = data_folder
+        self.ca_video_tdms = ca_video_tdms
+        self.ca_video_path = ca_video_path
+        self.fps = fps
+        self.extract_frame_times = extract_frame_times
+        self.logging = logging
 
-    
+        # extra additional params
+        self.analog_inputs_tdms = kwargs.pop('analog_inputs_tdm', None)
+        self.camera_triggers_channel = kwargs.pop('camera_triggers_channe', None)
+        self.bue_led_triggers_channel = kwargs.pop('bue_led_triggers_channe', None)
+        self.violet_led_triggers_channel = kwargs.pop('violet_led_triggers_channe', None)
 
+        self.setup()
 
+    def setup(self):
+        """
+            Gets complete paths to the various files and checks that they exist and starts fancy log logger
+        """
+        # Get complete file paths
+        if self.ca_video_path is None:
+            self.ca_video_path = os.path.join(self.data_folder, get_file_name(self.ca_video_tdms)+".mp4")
+        self.ca_video_tdms = os.path.join(self.data_folder, self.ca_video_tdms)
+        self.ca_video_metadata_tdms = os.path.join(self.data_folder, get_file_name(self.ca_video_tdms)+"meta.tdms")
 
-# ---------------------------------------------------------------------------- #
-#                                    PARAMS                                    #
-# ---------------------------------------------------------------------------- #
+        if self.extract_frame_times:
+            self.analog_inputs_tdms = os.path.join(self.data_folder, self.analog_inputs_tdms)
+            check_file_exists(self.analog_inputs_tdms, raise_error=True)
 
-# --------------------------------- Variables -------------------------------- #
+        # Start logging
+        if not self.logging:
+            fancylog.start_logging(self.data_folder, package, verbose=True, filename="fp_preprocessing",
+                    multiprocessing_aware=False, write_cli_args=False)
+            logging.info("Starting to preprocess files:")
+            logging.info(self.data_folder, self.ca_video_tdms, self.ca_video_metadata_tdms, self.ca_video_path)
 
-data_folder = "Z:\\swc\\branco\\rig_photometry\\tests\\200205\\200205_mantis_test_longer_exposure_noaudio"
-ca_video_tdms = "FP_calcium_and_behaviour(0)-FP_calcium_camera.tdms"
-ca_video_path = None # If left as None it will save it the same folder and with the same name as the tdms file
-FPS = 100
-# ---------------------------------- Options --------------------------------- #
+        # check files exist
+        video_exists = check_file_exists(self.ca_video_tdms)
+        if not video_exists:
+            logging.warning("Couldn't find calcium video tdms at: " + self.ca_video_tdms)
+            raise FileNotFoundError("Couldn't find calcium video tdms at: " + self.ca_video_tdms)
 
-OVERWRITE = False # If true it will overwrite the results of a previous analysis
-RUN_CONVERSION = True # If true it will convert the tdms video to mp4
-EXTRACT_FRAME_TIMES = True # If true it will extract the time of blue and violed LEDs frames from analog inputs (slow)
+        metadata_exists = check_file_exists(self.ca_video_metadata_tdms)
+        if not metadata_exists:
+            logging.warning("Couldn't find calcium metadata tdms at: " + self.ca_video_metadata_tdms)
+            raise FileNotFoundError("Couldn't find calcium metadata tdms at: " + self.ca_video_metadata_tdms+
+                    "\nPipeline assumes that metadata and video files are saved in the same data_folder")
 
+    def run(self):
+        """
+            Runs the pipeline
+        """
+        cameraname, experiment_name = self.check_for_dropped_frames()
 
-# ------------------------------ Extra variables ----------------------------- #
-# ? To extrac frame times
-analog_inputs_tdms = "FP_calcium_and_behaviour(0).tdms"
-camera_triggers_channel='FP_calciumcamera_triggers_reading'
-bue_led_triggers_channel='camera_triggers_channel'
-violet_led_triggers_channel='camera_triggers_channel'
+        if self.run_conversion:
+            self.convert_video_tdms()
 
+        # If the converted video doesn't exist, we can't correct the rotation
+        if not check_file_exists(self.ca_video_path):
+            logging.warning("No converted video path exists, terminating analysis")
+            raise FileExistsError("No converted video analysis exists, please convert video \
+                            or set run_conversion=True")
 
-
-def setup
-# ---------------------------------------------------------------------------- #
-#                                     SETUP                                    #
-# ---------------------------------------------------------------------------- #
-# Get complete file paths
-if ca_video_path is None:
-    ca_video_path = os.path.join(data_folder, get_file_name(ca_video_tdms)+".mp4")
-ca_video_tdms = os.path.join(data_folder, ca_video_tdms)
-ca_video_metadata_tdms = os.path.join(data_folder, get_file_name(ca_video_tdms)+"meta.tdms")
-
-if EXTRACT_FRAME_TIMES:
-    analog_inputs_tdms = os.path.join(data_folder, analog_inputs_tdms)
-    check_file_exists(analog_inputs_tdms, raise_error=True)
-
-# Start logging
-
-fancylog.start_logging(data_folder, package, verbose=True, filename="fp_preprocessing",
-        multiprocessing_aware=False, write_cli_args=False)
-logging.info("Starting to preprocess files:")
-logging.info(data_folder, ca_video_tdms, ca_video_metadata_tdms, ca_video_path)
-
-
-
-
-
-
-# ---------------------------------------------------------------------------- #
-#                           CHECK EVERYTHING IS OKAY                           #
-# ---------------------------------------------------------------------------- #
-
-# ----------------------------- Check files exist ---------------------------- #
-video_exists = check_file_exists(ca_video_tdms)
-if not video_exists:
-    raise FileNotFoundError("Couldn't find calcium video tdms at: " + ca_video_tdms)
-
-metadata_exists = check_file_exists(ca_video_metadata_tdms)
-if not metadata_exists:
-    raise FileNotFoundError("Couldn't find calcium metadata tdms at: " + ca_video_metadata_tdms+
-            "\nPipeline assumes that metadata and video files are saved in the same data_folder")
-
-
-# --------------------------- Check dropped frames --------------------------- #
-if check_file_exists(ca_video_path) and not overwrite:
-    logging.info("Found a converted video file, skipping dropped frames checked and conversion")
-else:
-    logging.info("Checking dropped frames...")
-    camera_name = ca_video_tdms.split("(0)-")[-1].split(".tdms")[0]
-    experiment_name = ca_video_tdms.split("(0)-")[0]
-    notdropped = check_mantis_dropped_frames(data_folder, camera_name, experiment_name, 
-                            skip_analog_inputs=True, verbose=False)
-    if notdropped:
-        logging.info("No frames were dropped")
-
-    # ? tdms -> conversion
-    if RUN_CONVERSION:
-        logging.info("Converting .tdms video to .mp4")
-        convert(ca_video_tdms, ca_video_metadata_tdms, fps=FPS, output_path=ca_video_path)
-        logging.info("Video converted")
-
-    
+        # Extract timing of blue and violet led frames from the analog inputs
+        if self.extract_frame_times:
+            frame_starts = self.extract_frames(experiment_name)
+        else:
+            frame_starts = None
 
 
-# ---------------------------------------------------------------------------- #
-#                               CORRECT ROTATION                               #
-# ---------------------------------------------------------------------------- #
+    def check_for_dropped_frames(self):
+        """
+            Checks if dropped frames were dropped during the recording
+        """
+        logging.info("Checking dropped frames...")
+        camera_name = self.ca_video_tdms.split("(0)-")[-1].split(".tdms")[0]
+        experiment_name = self.ca_video_tdms.split("(0)-")[0]
+        notdropped = check_mantis_dropped_frames(self.data_folder, camera_name, experiment_name, 
+                                skip_analog_inputs=True, verbose=False)
+        if notdropped:
+            logging.info("No frames were dropped")
 
-# TODO make code to correct the rotation of the fiber bundle. 
-# Waiting for final cables before testing this. 
+        return camera_name, experiment_name
 
-"""
-    Likely we will use either bespoke code or DLC to find a number of points in the image, 
-    then use the common coordinates behaviour to rotate to a template fiber.
+                
+    def convert_video_tdms(self):
+        """
+            Converts video .tdms to mp4.
+        """
+        if check_file_exists(self.ca_video_path) and not self.overwrite:
+            logging.info("Converted video file exists already, skipping conversion")
+        else:
+            logging.info("Converting .tdms video to .mp4")
+            convert(self.ca_video_tdms, self.ca_video_metadata_tdms, fps=self.fps, output_path=self.ca_video_path)
+            logging.info("Video converted, saved at: {}".format(self.ca_video_path))
 
-    Then from the registered image just use pre-determined ROIs to extract fluorescence.
+    def correct_fiberbundle_rotation(self):
+        # TODO make code to correct the rotation of the fiber bundle. 
+        # Waiting for final cables before testing this. 
 
-    Need to find ways to validate this, e.g. generate N random frames with template and frame overlayed
-"""
+        """
+            Likely we will use either bespoke code or DLC to find a number of points in the image, 
+            then use the common coordinates behaviour to rotate to a template fiber.
 
+            Then from the registered image just use pre-determined ROIs to extract fluorescence.
 
+            Need to find ways to validate this, e.g. generate N random frames with template and frame overlayed
+        """
 
-
-# ---------------------------------------------------------------------------- #
-#                              EXTRACT FRAME TIMES                             #
-# ---------------------------------------------------------------------------- #
-if EXTRACT_FRAME_TIMES:
+    def extract_frames(self, experiment_name):
         logging.info("Extracting frame times.")
 
-        frames_times_file = os.path.join(data_folder, experiment_name+"frame_times.yml")
+        self.frames_times_file = os.path.join(self.data_folder, experiment_name+"frame_times.yml")
 
-        if check_file_exists(frames_times_file) and not overwrite:
+        if check_file_exists(self.frames_times_file) and not self.overwrite:
             logging.info("Frames times file exists already, skipping. ")
         else:
             logging.info("No frames times file found, running analysis")
 
         # Load analog inputs
-        inputs = get_analog_inputs_clean_dataframe(analog_inputs_tdms, is_opened=False)
+        inputs = get_analog_inputs_clean_dataframe(self.analog_inputs_tdms, is_opened=False)
 
         # Extract frame start time
         frame_starts = {}
-        for name, channel in zip(['calcium_camera', 'blue_led', 'violet_led'], [camera_triggers_channel, bue_led_triggers_channel, violet_led_triggers_channel]):
+        for name, channel in zip(['calcium_camera', 'blue_led', 'violet_led'], 
+                        [self.camera_triggers_channel, self.bue_led_triggers_channel, self.violet_led_triggers_channel]):
             frame_starts[name] = get_frames_times_from_squarewave_signal(inputs[channel].values, debug=False)
 
         # Save
-        save_yaml(frames_times_file, content, append=False)
+        save_yaml(self.frames_times_file, frame_starts, append=False)
+        logging.info("Saved frame times at self.frames_times_file")
+        return frame_starts
+
+
+
+if __name__ == '__main__':
+    data_folder = "Z:\\swc\\branco\\rig_photometry\\tests\\200205\\200205_mantis_test_longer_exposure_noaudio"
+    ca_video_tdms = "FP_calcium_and_behaviour(0)-FP_calcium_camera.tdms"
+    ca_video_path = None # If left as None it will save it the same folder and with the same name as the tdms file
+    FPS = 100
+
+    OVERWRITE = False # If true it will overwrite the results of a previous analysis
+    RUN_CONVERSION = True # If true it will convert the tdms video to mp4
+    EXTRACT_FRAME_TIMES = True # If true it will extract the time of blue and violed LEDs frames from analog inputs (slow)
+
+    # ? To extrac frame times
+    analog_inputs_tdms = "FP_calcium_and_behaviour(0).tdms"
+    camera_triggers_channel='FP_calciumcamera_triggers_reading'
+    bue_led_triggers_channel='camera_triggers_channel'
+    violet_led_triggers_channel='camera_triggers_channel'
 
 
